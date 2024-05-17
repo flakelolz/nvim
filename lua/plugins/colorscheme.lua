6bf9cf239c6b162a00d44fbc83d3c5da2fb63352
Michael Gonzalez <michaelgonzalez.code@gmail.com>
2024-05-17 00:52:23 -0400

    No need for tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,12 @@
 return {
 
   -- Add themes
-  {},
+  {
+    {
+      "folke/tokyonight.nvim",
+      enabled = false,
+    },
+  },
   -- Configure LazyVim to load themes
   {
     "LazyVim/LazyVim",
