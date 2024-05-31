504a1c060c8b2946a6f84f7d94bad5340757186d
Michael Gonzalez <michaelgonzalez.code@gmail.com>
2024-05-31 12:58:28 -0400

    Made mini.pairs ignore &' to make typing Rust lifetimes easier

---
+++ lua/plugins/mini-pairs.lua
@@ -0,0 +1,9 @@
+return {
+  "echasnovski/mini.pairs",
+  event = "VeryLazy",
+  opts = {
+    mappings = {
+      ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a&\\].", register = { cr = false } },
+    },
+  },
+}
