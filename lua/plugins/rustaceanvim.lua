4f94442c6f02c0503f223779eb2b0e8a3c24d5b4
Michael Gonzalez <michaelgonzalez.code@gmail.com>
2024-05-31 12:36:26 -0400

    Rounded borders for Rust

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,41 +1,9 @@
 return {
   "mrcjkb/rustaceanvim",
   opts = {
-    server = {
-      default_settings = {
-        ["rust-analyzer"] = {
-          semanticHighlighting = {
-            doc = {
-              comment = {
-                inject = {
-                  enable = false,
-                },
-              },
-            },
-            nonStandardTokens = false,
-
-            operator = {
-              enable = false,
-              specialization = {
-                enable = true,
-              },
-            },
-            punctuation = {
-              enable = true,
-              separate = {
-                macro = {
-                  bang = true,
-                },
-              },
-              specialization = {
-                enable = true,
-              },
-            },
-            strings = {
-              enable = false,
-            },
-          },
-        },
+    tools = {
+      float_win_config = {
+        border = "rounded",
       },
     },
   },
