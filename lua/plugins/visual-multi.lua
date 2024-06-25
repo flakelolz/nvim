2a57019fcaac03df684c4b7f0537b15ce956a32f
Michael Gonzalez <michaelgonzalez.code@gmail.com>
2024-06-25 02:03:16 -0400

    Fixed vim-visual-multi keybind clash and disabled headlines.nvim

--- lua/plugins/visual-multi.lua
+++ lua/plugins/visual-multi.lua
@@ -1,10 +1,29 @@
 return {
   "mg979/vim-visual-multi",
+  init = function()
+    vim.g.VM_default_mappings = 0
+    vim.g.VM_maps = {
+      ["Find Under"] = "<C-n>",
+      ["Select Cursor Up"] = "<M-C-Up>",
+      ["Select Cursor Down"] = "<M-C-Down>",
+    }
+    vim.g.VM_add_cursor_at_pos_no_mappings = 1
+  end,
   keys = {
     {
       "<C-n>",
       mode = { "n", "x", "v" },
-      desc = "Next Multi",
+      desc = "VM Find Under",
+    },
+    {
+      "<M-C-Up>",
+      mode = { "n", "x", "v" },
+      desc = "VM Select Cursor Up",
+    },
+    {
+      "<M-C-Down>",
+      mode = { "n", "x", "v" },
+      desc = "VM Select Cursor Down",
     },
   },
 }
