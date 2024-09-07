12d9cf90cf62c3d3ecaae38ced64f13423937e15
Michael Gonzalez <michaelgonzalez.code@gmail.com>
2024-09-07 00:57:33 -0400

    tailwind class sorter and colorizer

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,9 @@
+return {
+
+  "NvChad/nvim-colorizer.lua",
+  ft = { "css", "scss", "html", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  opts = {
+    filetypes = { "css", "scss", "html", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+    user_default_options = { mode = "virtualtext" },
+  },
+}
