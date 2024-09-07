2ee8c28e072fac9d301753049e43087a350e081b
Michael Gonzalez <michaelgonzalez.code@gmail.com>
2024-09-07 01:15:51 -0400

    Better colorizer settings

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -3,7 +3,20 @@ return {
   "NvChad/nvim-colorizer.lua",
   ft = { "css", "scss", "html", "javascript", "javascriptreact", "typescript", "typescriptreact" },
   opts = {
-    filetypes = { "css", "scss", "html", "javascript", "javascriptreact", "typescript", "typescriptreact" },
-    user_default_options = { mode = "virtualtext" },
+    filetypes = {
+      "css",
+      "scss",
+      "html",
+      "javascript",
+      "typescript",
+      javascriptreact = { mode = "background" },
+      typescriptreact = { mode = "background" },
+    },
+    user_default_options = {
+      tailwind = true,
+      mode = "virtualtext",
+      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+    },
   },
 }
