df5b1c52002830c4531145ee84170f2e0178a048
Michael Gonzalez <michaelgonzalez.code@gmail.com>
2024-10-07 12:58:52 -0400

    vtsls ignore ES Module & CommonJS stuff with imports

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,6 +4,23 @@ return {
     -- Can toggle inlay hints with <leader>uh
     inlay_hints = { enabled = true },
     servers = {
+      vtsls = {
+        handlers = {
+          ["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
+            if result.diagnostics ~= nil then
+              local idx = 1
+              while idx <= #result.diagnostics do
+                if result.diagnostics[idx].code == 80001 then
+                  table.remove(result.diagnostics, idx)
+                else
+                  idx = idx + 1
+                end
+              end
+            end
+            vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+          end,
+        },
+      },
       pyright = {
         settings = {
           python = {
