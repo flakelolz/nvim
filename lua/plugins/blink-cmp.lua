65dd22243c417b2aab47f151fe7292a15a158f89
flakelolz <michaelgonzalez.code@gmail.com>
2024-12-19 13:37:24 -0400

    new lazyvim with blink.cmp and fzf-lua

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,21 @@
+return {
+  "Saghen/blink.cmp",
+  opts = {
+    window = {
+      border = "rounded",
+    },
+    menu = {
+      border = "rounded",
+    },
+    documentation = {
+      window = {
+        border = "rounded",
+      },
+    },
+    keymap = {
+      ["<C-Space>"] = {},
+      preset = "default",
+      ["<C-K>"] = { "show", "show_documentation", "hide_documentation" },
+    },
+  },
+}
--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,6 +1,7 @@
 return {
   {
     "Exafunction/codeium.vim",
+    enabled = false,
     event = "BufEnter",
     version = "1.8.37",
     config = function()
