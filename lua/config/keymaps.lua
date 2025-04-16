18b07a02a17b1284a6a16c081f17ae57409fa9a5
Michael Gonzalez <michaelgonzalez.code@gmail.com>
2025-04-16 06:53:28 -0400

    void paste and delete + other

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,11 +7,14 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Jump Down" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Jump Up" })
 vim.keymap.set("n", "<C-o>", "<C-o>zz", { desc = "Jump Back" })
 vim.keymap.set("n", "<C-i>", "<C-i>zz", { desc = "Jump Forward" })
+-- Leaves search terms in the middle
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search term" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search term" })
 -- Leaves cursor at the same location instead of the end of the line
 vim.keymap.set("n", "J", "mzJ`z", { desc = "Join next line" })
 
--- stylua: ignore
 -- Inlay end hints
+-- stylua: ignore
 vim.keymap.set("n", "<leader>uH", function() require("lsp-endhints").toggle() end, { desc = "Toggle end inlay hints" })
 
 -- Delete default floating terminal
@@ -20,13 +23,23 @@ vim.keymap.del("n", "<C-_>")
 vim.keymap.del("t", "<C-/>")
 vim.keymap.del("t", "<C-_>")
 
+-- Remap floating terminal
 -- stylua: ignore
 vim.keymap.set("n", "<M-;>", function() Snacks.terminal(nil, { cwd = LazyVim.root() }) end, { desc = "Terminal (Root Dir)" })
 -- stylua: ignore
 vim.keymap.set("t", "<M-;>", function() Snacks.terminal(nil, { cwd = LazyVim.root() }) end, { desc = "Terminal (Root Dir)" })
 
 -- Comments - Because of how a terminal might be sending the input, need both / and _ keymaps
-vim.keymap.set("n", "<C-/>", "gcc", { desc = "Comment line", remap = true })
-vim.keymap.set("n", "<C-_>", "gcc", { desc = "Comment line", remap = true })
-vim.keymap.set("v", "<C-/>", "gc", { desc = "Comment block", remap = true })
-vim.keymap.set("v", "<C-_>", "gc", { desc = "Comment block", remap = true })
+vim.keymap.set("n", "<C-/>", "gcc", { desc = "Comment line" })
+vim.keymap.set("n", "<C-_>", "gcc", { desc = "which_key_ignore" })
+
+vim.keymap.set("v", "<C-/>", "gc", { desc = "Comment block" })
+vim.keymap.set("v", "<C-_>", "gc", { desc = "which_key_ignore" })
+
+-- Send highlighted text to void register before pasting
+vim.keymap.set("x", "<leader>p", '"_dP', { desc = "Void paste" })
+-- Delete text and send to void register
+vim.keymap.set({ "n", "v", "x" }, "<M-d>", '"_d', { desc = "Void delete" })
+
+-- Make C-c same as Esc
+vim.keymap.set("i", "<C-c>", "<Esc>")
