ac98a8a74cfca528fdc65ffb791f530ef564468c
flakelolz <michaelgonzalez.code@gmail.com>
2025-04-28 19:43:04 -0400

    added borders to lsp diagnostics

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,5 +3,10 @@ return {
   opts = {
     -- Can toggle inlay hints with <leader>uh
     inlay_hints = { enabled = true },
+    diagnostics = {
+      float = {
+        border = "rounded",
+      },
+    },
   },
 }
