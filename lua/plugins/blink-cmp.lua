b42ffdb511abca34966a7c9ae98d7e2dd18e69f1
flakelolz <michaelgonzalez.code@gmail.com>
2025-05-01 19:56:38 -0400

    added another signature help when typing arguments

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -5,7 +5,7 @@ return {
       menu = { border = "rounded" },
       documentation = { window = { border = "rounded" } },
     },
-    signature = { window = { border = "rounded" } },
+    signature = { enabled = true, window = { border = "rounded" } },
     keymap = {
       ["<C-Space>"] = {},
       preset = "default",
