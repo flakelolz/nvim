6f556d1eb0bea76f65e3a1a5b9c673ce1e601524
flakelolz <michaelgonzalez.code@gmail.com>
2025-05-21 11:42:44 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -1,25 +1,7 @@
 {
-  description = "My Neovim configuration";
+  description = "My Neovim config";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self }: {
+    configDir = self;
   };
-
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        packages.default = pkgs.stdenv.mkDerivation {
-          pname = "neovim-config";
-          version = "1.0.0";
-          src = ./.;
-
-          installPhase = ''
-            mkdir -p $out
-            cp -r * $out/
-          '';
-        };
-      });
 }
